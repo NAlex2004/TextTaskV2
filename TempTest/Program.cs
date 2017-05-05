5c5dab9fc49f4717758f415fe171c2e09bf601bd
NAlex2004 <nalex2004@gmail.com>
2017-05-05 16:23:42 +0300

    .

--- TempTest/Program.cs
+++ TempTest/Program.cs
@@ -7,6 +7,7 @@ using NAlex.TextModel.Factories;
 using System.IO;
 using System.Linq;
 using System.Text;
+using NAlex.TextModel.Extensions;
 
 namespace TempTest
 {
@@ -23,6 +24,7 @@ namespace TempTest
             IWord w2 = new Word(new [] { s1, s2 });
             ISentence sentence = new Sentence();
             sentence.Add(w1);
+            sentence.Add(new Space());
             sentence.Add(w2);
             sentence.Add(new Space());
             sentence.Add(p1);
@@ -38,7 +40,11 @@ namespace TempTest
             //model.SelectMany(s => s.Select(i => i.Value)).ToList().ForEach(s => Console.WriteLine(s));
             //foreach (var sent in model)
             //    Console.Write(sent);
+
+            // по возрастанию кол-ва слов
             model.OrderBy(s => s.OfType<Word>().Count()).ToList().ForEach(s => Console.WriteLine("{0} {1}", s.OfType<Word>().Count(), s));
+
+            // в вопросительных слова заданной длины без повторений
             var q = model.Where(s => s.ToString().EndsWith("?")).SelectMany(s => s.OfType<Word>()).Where(w => w.Length == 3)
                 //.GroupBy(w => w.Value)
                 //.Select(g => g.Key);
@@ -46,19 +52,24 @@ namespace TempTest
                 .Distinct();            
 
             foreach (var w in q)
-                Console.WriteLine(w.Text);
+                Console.WriteLine(w.Text);            
 
-            var words = model.SelectMany(s => s.OfType<Word>()).Where(w => w.Length == 3).ToArray(); // еще согласные проверить
+            // замена слов заданной длины на подстроку (предложение)
+            model.OrderBy(s => s.OfType<Word>().Count()).LastOrDefault().Replace(i => i.Value.Length == 5, sentence);
+            model.OrderBy(s => s.OfType<Word>().Count()).ToList().ForEach(s => Console.WriteLine("{0} {1}", s.OfType<Word>().Count(), s));
+
+            // удаление на согласную
+            var words = model.SelectMany(s => s.OfType<Word>()) 
+                .Where(w => w.Length == 4 && !w[0].Value[0].IsVowel())
+                .ToArray();
             foreach (var w in words)
             {
                 foreach (var s in model)
                     s.Remove(w);
             }
+
             model.OrderBy(s => s.OfType<Word>().Count()).ToList().ForEach(s => Console.WriteLine("{0} {1}", s.OfType<Word>().Count(), s));
 
-            // замена
-            model.OrderBy(s => s.OfType<Word>().Count()).LastOrDefault().Replace(i => i.Value.Length == 5, sentence);
-            model.OrderBy(s => s.OfType<Word>().Count()).ToList().ForEach(s => Console.WriteLine("{0} {1}", s.OfType<Word>().Count(), s));
             Console.ReadKey();
         }
     }
---
+++ TextModel/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAlex.TextModel.Interfaces;
+using NAlex.TextModel.Model;
+
+namespace NAlex.TextModel.Extensions
+{
+    public static class Extensions
+    {
+        static char[] vowels = { 'e', 'y', 'u', 'i', 'o', 'a', 
+                                'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю', 'ё'};
+        public static bool IsVowel(this char ch)
+        {
+            char c = char.ToLower(ch);
+            bool res = vowels.Contains(char.ToLower(ch));
+            return vowels.Contains(char.ToLower(ch));
+        }
+    }
+}
